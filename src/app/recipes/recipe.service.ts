7fcf720aa9544697f1e4a25e2ce43adb740002b3
mfarhankz <m.farhankz@gmail.com>
2016-09-21 02:00:51 +0500

    add services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe';
+import { Ingredient } from '../shared/ingredient';
+
+@Injectable()
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/12_powerhouse_vegetables_slideshow/intro_cream_of_crop.jpg', [
+        new Ingredient('French Fries', 5),
+        new Ingredient('Samosa', 5)
+    ]),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Samosa', 1)
+    ])
+  ];
+
+  constructor() { }
+
+  getRecipes(){
+    return this.recipes
+  }
+
+}
