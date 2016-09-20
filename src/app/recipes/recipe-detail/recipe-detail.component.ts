696f24956968edb6e3949bf41db582d0253b1f51
mfarhankz <m.farhankz@gmail.com>
2016-09-21 02:00:24 +0500

    update

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from "../recipe";
+import { ShoppingListService } from "../../shopping-list/shopping-list.service";
 
 @Component({
-  selector: 'app-recipe-detail',
+  selector: '[app-recipe-detail]',
   templateUrl: './recipe-detail.component.html'
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) {}
 
   ngOnInit() {
+  }
+
+  onAddToShoppingList(){
+    this.sls.addItems(this.selectedRecipe.ingredients);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {ShoppingListService} from "./shopping-list.service";
+import {Ingredient} from "../shared/ingredient";
 
 @Component({
   selector: '[app-shopping-list]',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
-
-  constructor() { }
+  items: Ingredient[] = [];
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+    this.items = this.sls.getItems();
   }
+
 
 }
