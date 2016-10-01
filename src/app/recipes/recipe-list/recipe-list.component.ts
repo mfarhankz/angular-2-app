ce3a236b6bb4db26ca1dd437fd38d0a74fbd86bb
mfarhankz <m.farhankz@gmail.com>
2016-10-02 02:11:45 +0500

    add code in list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe';
-import {RecipeService} from "../recipe.service";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: '[app-recipe-list]',
@@ -13,6 +13,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+    this.recipeService.recipesChanged.subscribe(
+        (recipes: Recipe[]) => this.recipes = recipes
+    );
   }
 
 
